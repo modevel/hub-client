c3fca2c108132508417d6870558d51c222aab847
Martin Oppitz <github@martinoppitz.com>
2020-10-05 06:08:10 +0200

    fix: demo backend url

--- src/shares/register.ts
+++ src/shares/register.ts
@@ -4,7 +4,9 @@ import { DI } from '@leanup/lib/helpers/injector';
 import { MeasurementService } from '../services/measurements/service';
 import { StorageService } from '../services/storage/service';
 
-const ConfigApi = new Configuration({});
+const ConfigApi = new Configuration({
+  basePath: 'http://api.dipa.leanupjs.org/dipa/hub/0.1',
+});
 
 DI.register('StatusApi', new StatusApi(ConfigApi));
 
